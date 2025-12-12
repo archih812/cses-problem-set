3bb002607df307882d81cd59256bb2372e834e77
Archi Halder <archih812@gmail.com>
2025-12-12 21:10:11 +0530

    add: increasing array

---
+++ introductory_problems/increasing_array.py
@@ -0,0 +1,8 @@
+n = int(input())
+arr = list(map(int, input().split()))
+res = 0
+for i in range(1, len(arr)):
+    if arr[i] < arr[i - 1]:
+        res += arr[i - 1] - arr[i]
+        arr[i] = arr[i - 1]
+print(res)
